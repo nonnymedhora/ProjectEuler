8a3ec6d3217149c063ff9a3c6c488dc807c8cfb8
nonnymedhora <nonnymedhora@github.com>
2019-03-24 01:10:01 +0530

    Updates

--- src/org/bawaweb/euler/LargestPalindromeProduct.java
+++ src/org/bawaweb/euler/LargestPalindromeProduct.java
@@ -22,28 +22,37 @@ public class LargestPalindromeProduct {
 
 	public static void main(String[] args) {
 		int product = 0;
-		int top = (999*999);
-		int floor = (100*100);
+		final int LIMIT = 999;
+		final int BASE = 100;
+		int top = (LIMIT*LIMIT);
+		int floor = (BASE*BASE);
 		
-		int i = 999;
-		int j = 999;
+		int i = LIMIT;
+		int j = LIMIT;
 		boolean found = false;
-		for( i = 999; i >= 100; i--) {//System.out.println("i is "+i);
-			for(j = 999; j >= 100; j--) {
-				product = (i * j);//System.out.println("prod is "+product);
-				String str = String.valueOf(product);//System.out.println("star is "+str);
-				//System.out.println("For "+str+" "+j+" * "+i);
-				final StringBuilder reverse = new StringBuilder(str).reverse();
+		int max = 0;
+		int num1 = 0;
+		int num2 = 0;
+		
+		for( i = LIMIT; i >= BASE; i--) {
+			for(j = LIMIT; j >= BASE; j--) {
+				product = (i * j);
+				String str = String.valueOf(product);
+
+				final String reverse = new StringBuilder(str).reverse().toString();
 				if( str.equals(reverse)) {
-					System.out.println("star is "+str+" i is "+i+" j is "+j);
-					found = true;
-					break;
+					if(product > max){
+						max = product;
+						num1 = j;
+						num2 = i;
+						found = true;
+					}
 				}
 			}
 		}
 		if (found) {
 			System.out.println("Largest palindrome made from the product of two 3-digit numbers.");
-			System.out.println("is " + product + " = " + j + " * " + i);
+			System.out.println("is " + max + " = " + num1 + " * " + num2);
 		} else
 			System.out.println("No palindrome between "+floor+" and "+top);
 
