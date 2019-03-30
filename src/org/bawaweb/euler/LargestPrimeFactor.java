397092462843d8140858f46ced3f4d3a10a6c72e
nonnymedhora <nonnymedhora@github.com>
2019-03-31 04:14:07 +0530

    oooof Zeckenddorf!!!!!!!!!

--- src/org/bawaweb/euler/LargestPrimeFactor.java
+++ src/org/bawaweb/euler/LargestPrimeFactor.java
@@ -13,6 +13,8 @@ package org.bawaweb.euler;
  * Problem 3
  * The prime factors of 13195 are 5, 7, 13 and 29.
  * What is the largest prime factor of the number 600851475143 ?
+ * 
+ * ANS--6857
  *
  */
 	/*	OUTPUT
@@ -20,11 +22,10 @@ package org.bawaweb.euler;
 	 * Largest Prime Factor for 600851475143 is 300425737531
 	 **/
 public class LargestPrimeFactor {
-	static long target = 600851475143l;
+	static long target = 600851475143l;//13195;//
 	
 	private static boolean isPrime(final long number) {
 		boolean isPrime = true;
-
 		if (number < 0)
 			System.out.println("Invalid-Number	" + number);
 
@@ -32,50 +33,55 @@ public class LargestPrimeFactor {
 			return true;
 
 		for (long i = 2; i < ((long) Math.sqrt(number) + 1); i++) {
-			if (number % i == 0)
+			if (number % i == 0l)
 				return false;
 		}
-
 		return isPrime;
 	}
 	
 	
 	public static void main(String[] args) {
 		long largestPrime = 0l;
-		long half = ((target/2));
-		System.out.println("Target is "+ target+"    [and half is] "+half+"]");
+		long half = ((target/2));//System.out.println("t/29 is "+(target/29)+" and (t%29) is "+(target%29));
+		System.out.println("Target is "+ target+"    [and half is] ("+half+")");
 		
-		for(long x = half; x>1; x--) {
-			if(x%2==0){
-				continue;
-			}
-			if(x%3==0){
-				continue;
-			}
-			if(x%5==0){
-				continue;
-			}
-			if(x%7==0){
-				continue;
-			}
-			if(x%11==0){
-				continue;
-			}
-			if(x%13==0){
-				continue;
-			}
-			if(x%17==0){
-				continue;
-			}
-			if(x%19==0){
-				continue;
-			}
-			if(x%23==0){
-				continue;
-			}
-			if(isPrime(x)){
-				largestPrime = x;
-				break;
+		
+		for( long x = half; x > 5000; x-- ) {//System.out.println(x);
+			
+			if ( target%x == 0 ) {//System.out.println("IsFactor "+x);
+				
+				if (x % 2 == 0) {
+					continue;
+				}
+				if (x % 3 == 0) {
+					continue;
+				}
+				if (x % 5 == 0) {
+					continue;
+				}
+				if (x % 7 == 0) {
+					continue;
+				}
+				if (x % 11 == 0) {
+					continue;
+				}
+				if (x % 13 == 0) {
+					continue;
+				}
+				if (x % 17 == 0) {
+					continue;
+				}
+				if (x % 19 == 0) {
+					continue;
+				}
+				if (x % 23 == 0) {
+					continue;
+				}
+				
+				if ( isPrime(x) ) {//System.out.println("Testing Prime for "+x);
+					largestPrime = x;
+					break;
+				} 
 			}
 		}
 		
